e27ffd6e21e8d92e778d7478eb129baa25bc95df
Simon LANOS <simon.lanos99@gmail.com>
2021-09-22 15:54:33 +0200

    feat: add romanToDecimal tests

--- tests/RomanConverterTest.php
+++ tests/RomanConverterTest.php
@@ -27,5 +27,19 @@ class RomanConverterTest extends TestCase
     public function testRomanToDecimal(): void
     {
         $this->assertEquals(0, RomanConverter::romanToDecimal(''));
+        $this->assertEquals(1, RomanConverter::romanToDecimal('I'));
+        $this->assertEquals(2, RomanConverter::romanToDecimal('II'));
+        $this->assertEquals(4, RomanConverter::romanToDecimal('IV'));
+        $this->assertEquals(5, RomanConverter::romanToDecimal('V'));
+        $this->assertEquals(6, RomanConverter::romanToDecimal('VI'));
+        $this->assertEquals(9, RomanConverter::romanToDecimal('IX'));
+        $this->assertEquals(10, RomanConverter::romanToDecimal('X'));
+        $this->assertEquals(11, RomanConverter::romanToDecimal('XI'));
+        $this->assertEquals(16, RomanConverter::romanToDecimal('XVI'));
+        $this->assertEquals(20, RomanConverter::romanToDecimal('XX'));
+        $this->assertEquals(50, RomanConverter::romanToDecimal('L'));
+        $this->assertEquals(100, RomanConverter::romanToDecimal('C'));
+        $this->assertEquals(500, RomanConverter::romanToDecimal('D'));
+        $this->assertEquals(1000, RomanConverter::romanToDecimal('M'));
     }
 }
