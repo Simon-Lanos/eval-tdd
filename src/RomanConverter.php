0057fb81ebbe2bda01a6c760da3f1b82009dffb3
Simon LANOS <simon.lanos99@gmail.com>
2021-09-22 16:09:31 +0200

    feat: add direct implementation of romanToDecimal

--- src/RomanConverter.php
+++ src/RomanConverter.php
@@ -4,6 +4,17 @@ namespace App;
 
 class RomanConverter
 {
+    private const ROMAN_MAP = [
+        'M' => 1000,
+        'D' => 500,
+        'C' => 100,
+        'L' => 50,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1
+    ];
 
     public static function decimalToRoman(int $number): string
     {
@@ -47,6 +58,14 @@ class RomanConverter
 
     public static function romanToDecimal(string $roman): int
     {
-        return 0;
+        $return = 0;
+        foreach (self::ROMAN_MAP as $romanValue => $numberValue) {
+            if (strpos($roman, $romanValue) === 0) {
+                $return += $numberValue + self::romanToDecimal(substr($roman, strlen($romanValue)));
+                break;
+            }
+        }
+
+        return $return;
     }
 }
